641e34168dbe1a88d0fe43c676e03ba82ccfc692
Jonathan Adcock <jonny.adcock@gmail.com>
2020-04-11 19:19:41 +0100

    tidied models file

--- fenlandlp/models.py
+++ fenlandlp/models.py
@@ -1,45 +1,77 @@
+'''
+Models for the `fenlandlp` Django app
+'''
+
+
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
+from django.urls import reverse
+
 from accounts.models import Client
 
-STATUS = ((0, "Draft"), (1, "Publish"))
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="plan_posts"
-    )
-    updated_on = models.DateTimeField(auto_now=True)
+    '''
+    Defines database table structure for `Post` entries
+
+    Insert description of what a `Post` is for here
+    '''
+
+    DRAFT = 0
+    PUBLISHED = 1
+
+    STATUS_CHOICES = (
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published'))
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=DRAFT)
+    slug = models.SlugField(max_length=200, unique=True)
+    title = models.CharField(max_length=200, unique=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        ordering = ["-created_on"]
-
-    def __str__(self):
-        return self.title
+    class Meta: # pylint: disable=missing-class-docstring,too-few-public-methods
+        ordering = ['-created_on']
 
     def get_absolute_url(self):
-        from django.urls import reverse
+        '''
+        Returns the detail view url for a single `Post` entry
+        '''
 
-        return reverse("post_detail", kwargs={"slug": str(self.slug)})
+        return reverse('fenlandlp:post_detail', kwargs={"slug": self.slug})
+
+    def __str__(self):
+        '''
+        Defines the string returned by the `str()` method when called on a single `Post` entry
+        '''
+
+        return self.title
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)
-    body = models.TextField()
-    file = models.FileField(max_length=100, blank=True, null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
+    '''
+    Defines database table structure for `Comment` entries
 
-    class Meta:
-        ordering = ["created_on"]
+    Insert description of what a `Comment` is for here
+    '''
+
+    active = models.BooleanField(default=False)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)
+    file = models.FileField(max_length=100, blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta: # pylint: disable=missing-class-docstring,too-few-public-methods
+        ordering = ['created_on']
 
     def __str__(self):
+        '''
+        Defines the string returned by the `str()` method when called on a single `Comment` entry
+        '''
+
+
         return "Comment {} by {}".format(self.body, self.user)
-    
